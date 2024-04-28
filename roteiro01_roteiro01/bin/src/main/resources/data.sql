f3b4b01648a05a055eed085c1f8d15a29c63d6a4
Guilherme Cantoni <104282001+gscantoni@users.noreply.github.com>
2024-04-28 16:37:27 -0300

    Delete roteiro01_roteiro01 directory

--- roteiro01_roteiro01/bin/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS task;
-CREATE TABLE task (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      title VARCHAR(250) NOT NULL,
-                      description VARCHAR(250) NOT NULL,
-                      completed BOOLEAN
-);
-
