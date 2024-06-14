af4f1055b4363001bd7e4efa1762f82c8c354f74
Guilherme Cantoni <gcantoni@sga.pucminas.br>
2024-06-14 16:53:38 -0300

    arrumando projeto

--- roteiro01/roteiro01/src/test/java/com/labdessoft/roteiro01/Roteiro01ApplicationTests.java
+++ roteiro01/roteiro01/src/test/java/com/labdessoft/roteiro01/Roteiro01ApplicationTests.java
@@ -1,19 +1,7 @@
 package com.labdessoft.roteiro01;
 
-import com.labdessoft.roteiro01.entity.Task;
-
-import com.labdessoft.roteiro01.repository.TaskRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.time.LocalDate;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
